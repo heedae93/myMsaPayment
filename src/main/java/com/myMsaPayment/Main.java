be0925a60e847360c6cf2bb40db0dd3c26850cac
heedae93 <hdh89900@gmail.com>
2025-01-01 15:55:00 +0900

    프로젝트 생성

---
+++ src/main/java/com/myMsaPayment/Main.java
@@ -0,0 +1,12 @@
+package com.myMsaPayment;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
