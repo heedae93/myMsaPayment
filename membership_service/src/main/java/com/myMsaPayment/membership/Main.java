3a1244842b341a4f315c92b74f25581251391c23
heedae93 <hdh89900@gmail.com>
2025-01-01 17:55:46 +0900

    모듈 생성

--- membership_service/src/main/java/com/myMsaPayment/membership/Main.java
+++ membership_service/src/main/java/com/myMsaPayment/membership/Main.java
@@ -1,4 +1,4 @@
-package com.myMsaPayment;
+package com.myMsaPayment.membership;
 
 
 import org.springframework.boot.SpringApplication;
--- membership_service/src/main/java/com/myMsaPayment/membership/TestContoroller.java
+++ membership_service/src/main/java/com/myMsaPayment/membership/TestContoroller.java
@@ -1,17 +1,17 @@
-package com.myMsaPayment;
+package com.myMsaPayment.membership;
+
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequiredArgsConstructor
-public class TestController {
-
+public class TestContoroller {
 
     @GetMapping("/test")
     public String test() {
-        return "Hello World!";
+        return "test";
     }
+
 }
